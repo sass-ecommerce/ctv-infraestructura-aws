53569727490ae1d7c7069d8f6fbb861f91bf6f93
Gabriel Omar Berdejo Pareja <gabrielberdejo96@gmail.com>
2026-06-11 16:26:25 -0500

    chore(infra): append version suffix to lambda data source names

--- infra/main.tf
+++ infra/main.tf
@@ -55,11 +55,11 @@ module "dynamodb_users" {
 
 # ---- Cognito ----
 data "aws_lambda_function" "pre_token" {
-  function_name = "${var.project}-lambda-pre-token-${var.environment}"
+  function_name = "${var.project}-lambda-pre-token-${var.environment}-01"
 }
 
 data "aws_lambda_function" "post_confirmation" {
-  function_name = "${var.project}-lambda-user-post-confirmation-${var.environment}"
+  function_name = "${var.project}-lambda-user-post-confirmation-${var.environment}-01"
 }
 
 module "cognito" {
